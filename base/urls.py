9722d55bad99bf889437fe4153da48f039db1b5b
unknown <peizhi.yu@hotmail.com>
2023-07-29 09:29:31 +0800

    Add post page

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about/", views.about)
+    path("", views.index, name="home"),
+    path("about/", views.about),
+    path("post/<str:pk>", views.getPost, name="post_detail"),
+    path("post/<str:pk>/delete", views.deletePost, name="post_delete"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", { "name":"Aaron"})
+    posts = Post.objects.all()
+    return render(request, "index.html", { "posts": posts})
 
 def about(request):
     return render(request, "about.html")
+
+def getPost(request, pk):
+    post = Post.objects.get(id = pk)
+    return render(request, "post_detail.html", { "post": post })
+
+def deletePost(request, pk):
+    post = Post.objects.get(id = pk)
+    post.delete()
+    return redirect("home")
